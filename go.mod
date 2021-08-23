4804386bbbd3088c3946bdb5c9140276b8475307
Frank Mendez <frankmendezresources@gmail.com>
2021-08-23 13:17:14 +0800

    connected to database

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module ambassador
 
 go 1.15
 
-require github.com/gofiber/fiber/v2 v2.17.0 // indirect
+require (
+	github.com/gofiber/fiber/v2 v2.17.0 // indirect
+	gorm.io/driver/mysql v1.1.2 // indirect
+	gorm.io/gorm v1.21.13 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+    dsn := "root:root@tcp(db:3306)/ambassador"
+    _, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+    if err != nil {
+        panic(err.Error())
+    }
+
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
